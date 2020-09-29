10801000b1d22032f04f9a9ca1dbdb2cb7301020
Bastian Ilso <contact@bastianilso.com>
2020-09-29 10:18:37 +0200

    Initial Unity Project: Pupil Labs v1.22.7

---
+++ Assets/Plugins/Pupil/Scripts/Utils/AttachToMainCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AttachToMainCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Camera mainCamera;
+
+    void Start()
+    {
+        this.transform.SetParent(mainCamera.transform,false);
+    }
+}
